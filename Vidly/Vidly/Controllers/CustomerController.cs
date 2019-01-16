208d3520394a84a161df770b83badaec8937f42b
radwyd@gmail.com <radwyd@gmail.com>
2019-01-16 22:07:51 +0100

    Vidly
    Full Vidly

---
+++ Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private List<Customer> CustomerList = new List<Customer>()
+        {
+            new Customer(){Name = "Kazek",Id = 0},
+            new Customer(){Name = "Wladek", Id = 1}
+        };
+     
+        [Route("customer/show-customers")]
+        public ActionResult ShowCustomers()
+        {
+            return View(CustomerList);
+        }
+
+        [Route("customer/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            if (CustomerList.Any(s => s.Id == id))
+                return View(CustomerList.FirstOrDefault(c => c.Id == id));
+            return 
+        }
+
+        [Route("customer/{name}")]
+        public ActionResult CustomerByName(string name)
+        {
+            return View(CustomerList.FirstOrDefault(s => s.Name == name));
+        }
+    }
+}
